b04aff2d3e7c0b412a7554d542a682b0d6da198b
Ezri King <eking33@actx.edu>
2025-03-05 12:34:42 -0600

    Add project files.

---
+++ MidEx1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidEx1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Objective: Take overall grade input from user (0 to 100)
+            // Convert to a letter grade using a switch statement
+            
+
+            // processing
+
+
+            // declarations
+
+
+            
+        }
+    }
+}
